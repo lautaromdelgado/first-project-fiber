9951312758c496522567e3daae064eebb8a1d1a8
lautaromdelgado <lautaromdelgado@gmail.com>
2025-05-17 05:11:15 -0300

    correción en el error de no reconocer los endpoints

--- internal/entrypoint/server/fiber.go
+++ internal/entrypoint/server/fiber.go
@@ -4,10 +4,16 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/lautaromdelgado/first-project-fiber/internal/routes"
+	"github.com/lautaromdelgado/first-project-fiber/internal/security/middlewares"
 )
 
 func InitFiber() *fiber.App {
 	app := fiber.New()
+
+	app.Use(middlewares.LoggingMiddleware)
+
+	routes.InitRoutes(app)
 
 	log.Fatal(app.Listen(":3000"))
 
--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"github.com/lautaromdelgado/first-project-fiber/internal/entrypoint/server"
-	"github.com/lautaromdelgado/first-project-fiber/internal/security/middlewares"
 )
 
 func main() {
-	fiber := server.InitFiber()
-
-	fiber.Use(middlewares.LoggingMiddleware)
+	server.InitFiber()
 }
